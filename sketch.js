7338bdfe332210cc0f8c39cafe25a040e7f67278
SinghShaurya1 <104992391+SinghShaurya1@users.noreply.github.com>
2023-03-27 19:01:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function sendMessage() {
     var message = document.getElementsByClassName("chating")[0].value;
-    document.getElementsByClassName("chatBox")[0].innerHTML += "<p> " + message + "</p>";
+    document.getElementsByClassName("text")[0].innerHTML += "<p> " + message + "</p>";
     document.getElementsByClassName("chating")[0].value = "";
 }
